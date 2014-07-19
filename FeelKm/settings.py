9f64444b87cb758dc4b67225e541392f2283a183
Juli <jc.gutierrezq@gmail.com>
2014-07-19 12:07:43 -0500

    First commit of my django apsdsp

--- FeelKm/settings.py
+++ FeelKm/settings.py
@@ -115,7 +115,7 @@ STATIC_URL = 'statics/'
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+#   'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 
