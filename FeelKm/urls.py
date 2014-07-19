2e5166c7bc1161f81832d807c33cbd09124c27c4
Juli <jc.gutierrezq@gmail.com>
2014-07-19 14:45:03 -0500

    First commit of my django apsdssp

--- FeelKm/urls.py
+++ FeelKm/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
-#admin.autodiscover()
+admin.autodiscover()
 
 
 #from rest_framework import routers
