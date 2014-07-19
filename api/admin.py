8ecb5076bd6857b63221638f473c4648a1d65bfe
Juli <jc.gutierrezq@gmail.com>
2014-07-19 18:58:19 -0500

    First commit of my django apsdssp

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import events
+from .models import events, users
 
 admin.site.register(events)
+admin.site.register(users)
