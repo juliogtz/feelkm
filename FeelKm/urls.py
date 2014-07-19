a771300c5a5c1077866d0752b1c5acbb13d922cc
Juli <jc.gutierrezq@gmail.com>
2014-07-19 14:23:19 -0500

    First commit of my django apsdssp

--- FeelKm/urls.py
+++ FeelKm/urls.py
@@ -23,7 +23,7 @@ urlpatterns = patterns('',
     #url(r'^',include(router.urls)),
     #url(r'^events/$','api.views.event_list'),
     #url(r'^search/$','api.views.search'),
-    url(r'^admin/', include('admin.site.urls')),
+    url(r'^admin/', admin.site.urls),
     # websearch
     url(r'^search/','websearch.views.SearchWeb'),
 
