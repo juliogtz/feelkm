d671332978965c94003f83fbbd0a5815a500c118
Juli <jc.gutierrezq@gmail.com>
2014-07-19 12:38:04 -0500

    First commit of my django apsdsp

--- FeelKm/urls.py
+++ FeelKm/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
-
 from django.contrib import admin
 admin.autodiscover()
 
@@ -22,12 +21,11 @@ urlpatterns = patterns('',
     #url(r'^',include(router.urls)),
     #url(r'^events/$','api.views.event_list'),
     #url(r'^search/$','api.views.search'),
-    #url(r'^admin/', include(Fell)),
-    # websearch
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^search/','websearch.views.SearchWeb'),
 
     #url(r'^(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.STATIC_ROOT}),
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.STATIC_ROOT}),
+    (r'^statics/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.STATIC_ROOT}),
     #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
     # Autocomplete Search URL
