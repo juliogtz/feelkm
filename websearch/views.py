4201217f52f81ae84e40080f10438ed32276b674
Juli <jc.gutierrezq@gmail.com>
2014-07-20 14:12:52 -0500

    First commit of my django ssdsdsdssdsdp

--- websearch/views.py
+++ websearch/views.py
@@ -240,7 +240,7 @@ def CreateCommentSend(request):
                             #Save image:
                             fil=request.FILES[filename]
 
-                            destination = open("media", 'wb')
+                            destination = open("/media", 'wb')
                             for chunk in fil.chunks():
                                 destination.write(chunk)
                                 destination.close()
