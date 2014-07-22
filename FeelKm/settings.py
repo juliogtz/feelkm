4895f0045a0da7209ef65a45ab52e11ee65ff9d3
Juli <jc.gutierrezq@gmail.com>
2014-07-21 19:22:49 -0500

    First commit of my django ssdsdsdssdsdsp

--- FeelKm/settings.py
+++ FeelKm/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = (
 )
 
 
-AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_STORAGE_BUCKET_NAME = "feelkm-assets"
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 STATIC_URL = S3_URL
