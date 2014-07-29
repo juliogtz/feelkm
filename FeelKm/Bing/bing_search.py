3528682ac301e1a8b0dd44cee8fd8084dbcf9513
Juli <jc.gutierrezq@gmail.com>
2014-07-29 13:32:06 -0500

    First commit of my django ssdsdsdssdsdpss

--- FeelKm/Bing/bing_search.py
+++ FeelKm/Bing/bing_search.py
@@ -10,7 +10,7 @@ def bing_search(query, search_type):
     user_agent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)'
     credentials = (':%s' % key).encode('base64')[:-1]
     auth = 'Basic %s' % credentials
-    url = 'https://api.datamarket.azure.com/Data.ashx/Bing/Search/'+search_type+'?Query=%27'+query+'%27&$top=20&$format=json'
+    url = 'https://api.datamarket.azure.com/Data.ashx/Bing/Search/'+search_type+'?Query=%27'+query+'%27&$top=&$format=json'
     request = urllib2.Request(url)
     request.add_header('Authorization', auth)
     request.add_header('User-Agent', user_agent)
@@ -20,4 +20,4 @@ def bing_search(query, search_type):
     json_result = json.loads(response_data)
     result_list = json_result['d']['results']
     print result_list
-    return response_data
+    return result_list
