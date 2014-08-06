33593a6832709b233aa75ca5dccffe9311308c3d
Juli <jc.gutierrezq@gmail.com>
2014-08-05 19:11:16 -0500

    First commit of my django ssdsdsdssdsdps

--- websearch/views.py
+++ websearch/views.py
@@ -185,9 +185,10 @@ def SepecificEvent(request, id, year, month, day):
     if(number_comments>0):
         calif_4_count_p=(calif_4_count/number_comments)*100
         #calif_4_count_pix=(70*calif_4_count_p)/100
-        calif_4_count_pix=30
+        calif_4_count_pix=calif_4_count_p
+
     else:
-        calif_4_count_pix=10
+        calif_4_count_pix=0
 
 
     calif_5 = comments_events.objects.filter(calif=5, id_event=id)
