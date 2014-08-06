f921cd03395ffc7d35866c6b1f145ee7bb66b3f7
Juli <jc.gutierrezq@gmail.com>
2014-08-06 11:07:12 -0500

    First commit of my django ssdsdsdssdsdps

--- websearch/views.py
+++ websearch/views.py
@@ -132,8 +132,8 @@ def SepecificEvent(request, id, year, month, day):
     comments_arg=[]
 
     for co in com2:
-        usr_of= User.objects.filter(id=co.id_user_admin)
-        usr_aux= users.objects.filter(id_user_admin=co.id_user_admin)
+        usr_of= User.objects.get(id=co.id_user_admin)
+        usr_aux= users.objects.get(id_user_admin=co.id_user_admin)
         comments_arg.append({'id_facebook':usr_aux.id_facebook, 'pic_url':usr_aux.pic_url, 'city':usr_aux.city, 'region':usr_aux.region, 'country':usr_aux.country, 'first_name':usr_of.first_name, 'last_name':usr_of.last_name, 'comment':co })
 
 
