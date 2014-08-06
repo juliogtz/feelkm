c6b8de4d65e06492b6e9f0a3aff4b16ded5a6a1a
Juli <jc.gutierrezq@gmail.com>
2014-08-06 11:46:05 -0500

    First commit of my django ssdsdsdssdsdps

--- websearch/views.py
+++ websearch/views.py
@@ -132,10 +132,9 @@ def SepecificEvent(request, id, year, month, day):
     comments_arg=[]
 
     for cox in com2:
-        #usr_of= User.objects.filter(id=co.id_user_admin)
-        #usr_aux= users.objects.get(id_user_admin=co.id_user_admin)
-        #comments_arg.append({'id_facebook':usr_aux.id_facebook, 'pic_url':usr_aux.pic_url, 'city':usr_aux.city, 'region':usr_aux.region, 'country':usr_aux.country, 'first_name':usr_of.first_name, 'last_name':usr_of.last_name, 'comment':co })
-        comments_arg.append({'id_user_damin':cox.id_user_admin.first_name})
+        usr_aux= users.objects.get(id_user_admin=cox.id_user_admin)
+        comments_arg.append({'id_facebook':usr_aux.id_facebook, 'pic_url':usr_aux.pic_url, 'city':usr_aux.city, 'region':usr_aux.region, 'country':usr_aux.country, 'first_name':cox.id_user_admin.first_name, 'last_name':cox.id_user_admin.last_name, 'comment':cox })
+        #comments_arg.append({'id_user_damin':})
 
     # Califs
     calif_sum=0
