f768063fbd9807f8e106c2fa7c31044448dfb94a
Juli <jc.gutierrezq@gmail.com>
2014-08-07 12:20:05 -0500

    First commit of my django ssdsdsdssdsdps

--- websearch/templatetags/dictionary_extras.py
+++ websearch/templatetags/dictionary_extras.py
@@ -82,3 +82,18 @@ def shuffle(arg):
     file = arg[random.randrange(len(arg))]
     return file.file
 
+
+@register.filter(name='truncatechars_v')
+def truncatechars_v(s, num):
+    """
+    Truncates a word after a given number of chars
+    Argument: Number of chars to truncate after
+    """
+    length = int(num)
+    string = []
+    for word in s.split():
+        if len(word) > length:
+            string.append(word[:length]+'')
+        else:
+            string.append(word)
+    return u' '.join(string)
