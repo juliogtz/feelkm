296a9004775403891829a313150d940626c07db0
Juli <jc.gutierrezq@gmail.com>
2014-08-19 10:30:28 -0500

    First commit of my django ssdsdsdssdsdpss

--- FeelKm/urls.py
+++ FeelKm/urls.py
@@ -67,7 +67,7 @@ urlpatterns = patterns('',
 
     #Control de Favorites Events:
     url(r'favorites/','websearch.views.favorites'),
-    url(r'favorites-delete/(?P<id_event>.*)/$','usrs.views.delFavorites'),
+    url(r'favorites-delete/(?P<id_event>.*)/','usrs.views.delFavorites'),
 
 
 
